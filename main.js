729782f8d212a7e88df43aa0a6a29d0c168d5bf1
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-13 11:03:54 +0100

    handle close button

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
 
+// TOGGLE SIDEBAR
+toggleBtn.addEventListener('click', function() {
+  // console.log(sidebar.classList);
+  // if (sidebar.classList.contains('show-sidebar')) {
+  //   sidebar.classList.remove('show-sidebar');
+  // } else {
+  //   sidebar.classList.add('show-sidebar');
+  // }
+  sidebar.classList.toggle('show-sidebar');
+});
+
+// CLOSE BUTTON
+closeBtn.addEventListener('click', function() {
+  sidebar.classList.remove('show-sidebar');
+});
